3280be7f090f16abc07b340cce89a226e27b69c3
lukastuyettung <anhnguyen1122@gmail.com>
2026-07-04 13:15:51 +0700

    Create questions.js

---
+++ public/questions.js
@@ -0,0 +1,36 @@
+const questions = [
+  {
+    question: "Có công mài sắt, có ngày nên kim.",
+    answers: [
+      "Kiên trì sẽ thành công.",
+      "Muốn giàu phải làm nghề rèn.",
+      "Muốn giỏi phải thông minh."
+    ],
+    correct: 0,
+    explain: "Khuyên con người phải kiên trì, bền bỉ thì sẽ đạt được mục tiêu."
+  },
+
+  {
+    question: "Uống nước nhớ nguồn.",
+    answers: [
+      "Biết ơn những người đi trước.",
+      "Nên uống đủ nước mỗi ngày.",
+      "Luôn bảo vệ nguồn nước."
+    ],
+    correct: 0,
+    explain: "Nhắc nhở con người phải biết ơn cội nguồn và những người đã giúp đỡ mình."
+  },
+
+  {
+    question: "Một cây làm chẳng nên non, ba cây chụm lại nên hòn núi cao.",
+    answers: [
+      "Đoàn kết tạo nên sức mạnh.",
+      "Trồng nhiều cây sẽ có rừng.",
+      "Núi được tạo từ cây."
+    ],
+    correct: 0,
+    explain: "Đề cao tinh thần đoàn kết, hợp tác."
+  }
+];
+
+export default questions;
